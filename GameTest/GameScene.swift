275786720727f044753b292712d0bcf9e881b037
Salvatore Polito <thedarkhacker94@gmail.com>
2017-05-25 16:34:34 +0200

    ----BUMP Sostituiti gli SKNode con animazioni create attraverso il framework Lottie Cleanup del codice

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -9,84 +9,60 @@
 import SpriteKit
 import GameplayKit
 
+import Lottie
+
 class GameScene: SKScene {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
-    var listaComplimenti = ["sei bellissima ♥︎", "sei dolcissima", "le guanciotte sono morbidissime", "sei il mio cuoricino ♥︎", "hai le mani coccolosissime", "sei meravigliosa", "hai occhi dolcissimi", "sei cucciolosa"]
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
+    var listaComplimenti = ["sei bellissima ♥︎", "sei cucciolosa ♥︎", "sei dolcissima ♥︎", "sei meravigliosa ♥︎", "hai le mani coccolosissime", "le guanciotte sono morbidissime", "sei il mio cuoricino ♥︎", "hai occhi dolcissimi"]
+
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let index = GKRandomDistribution(lowestValue: 0, highestValue: (listaComplimenti.count - 1))
+        let num = index.nextInt()
         
+        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
         if let label = self.label {
-            label.text = listaComplimenti[index.nextInt()]
+            label.alpha = 0.0
+            label.text = listaComplimenti[num]
+            
+            if num > 3 {
+                label.fontSize = 55
+            } else {
+                label.fontSize = 80
+            }
+
             label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
         }
         
         for t in touches {
-            self.touchDown(atPoint: t.location(in: self))
-            let point = t.location(in: self)
+            //let point = t.location(in: self)
             
-            if let sprite = SKSpriteNode(imageNamed: "Spaceship") as? SKSpriteNode {
-                sprite.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-                sprite.position = point
-                self.addChild(sprite)
-            }
+            let animation = LOTAnimationView(name: "quick_hart_select")
+            animation?.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+            animation?.center = t.preciseLocation(in: self.view)
+            self.view?.addSubview(animation!)
             
+            animation?.play()
+            self.view?.subviews[0].removeFromSuperview()
         }
     }
- 
+    
 
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+}
+
+extension SKSpriteNode {
+    
+    func addGlow(radius: Float = 15) {
+        let effectNode = SKEffectNode()
+        effectNode.shouldRasterize = true
+        addChild(effectNode)
+        effectNode.addChild(SKSpriteNode(texture: texture))
+        effectNode.filter = CIFilter(name: "CIGaussianBlur", withInputParameters: ["inputRadius":radius])
     }
 }
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'GameTest' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for GameTest
+
+  target 'GameTestTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'GameTestUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  #Lottie framework
+  pod 'lottie-ios'
+
+end
